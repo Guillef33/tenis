9973cbba7651f5cbb86ec522bba696f237c3adac
Guillermo <guillef33@gmail.com>
2021-12-28 08:59:51 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './Home'
 import Post from './PostCard'
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Nav from './Nav'
-
+import Footer from "./Footer";
 
 function App() {
 
@@ -15,6 +15,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/posts/:id" element={<Post />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+//CSS
+import "./index.css";
+
+function Footer() {
+  return (
+    <footer className="footerContainer">
+      <div>
+        <h3>
+          Libreria independiente con textos en ingles. Importador oficial{" "}
+        </h3>
+      </div>
+
+    </footer>
+  );
+}
+
+export default Footer;
